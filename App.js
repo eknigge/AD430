9d2a91cac06f50ee3e9af67e3211f12bb36c85cc
David Nguyen <davidnguyen234@gmail.com>
2021-11-03 18:55:27 -0700

    add bottom sheet files

--- App.js
+++ App.js
@@ -1,24 +1,51 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions, useWindowDimensions,SafeAreaView,} from 'react-native';
+import { useSharedValue } from 'react-native-reanimated';
+import Overlay from './src/Overlay';
+import BottomSheet from './src/BottomSheet';
+import PicturesCarousel from './src/PicturesCarousel';
+
 
 export default function App() {
+    const { width, height } = useWindowDimensions();
+    const y = useSharedValue(0);
+    
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
-        </View>
-    );
+    
+            <MapView 
+            initialRegion={{
+                latitude: 47.6205,
+                longitude: -122.3493,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0922,
+              }}
+              style={{ width, height }}
+              >
+
+            </MapView>
+                
+
+            <Overlay panY={y} />
+
+            <PicturesCarousel panY={y} />
+
+            <BottomSheet panY={y} />
+            <SafeAreaView
+            style={StyleSheet.absoluteFill}
+            pointerEvents="none"
+            >
+            <View style={styles.container}>
+            </View>
+            </SafeAreaView>
+
+            </View>
+  );
 }
 
-const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+      flex: 1,
     },
-    map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-    },
-});
+  });
