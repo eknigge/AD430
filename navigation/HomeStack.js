2914ae56744631659e42fb2568d94fbc4fb00327
davidnguyen234 <47171828+davidnguyen234@users.noreply.github.com>
2021-11-20 18:19:34 -0800

    Signout (#46)
    * components/routes/Drawer.js
    
    * add side drawer files
    
    * add log off files
    
    Co-authored-by: Tizeta2018 <tesfutizeta@yahoo.com>
    Co-authored-by: shipitsteven <stevenwangbe@gmail.com>

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -3,46 +3,48 @@ import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import SignOut from '../src/SignOut';
 
 const Stack = createStackNavigator();
 
 function CreateTourScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
+    return (
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+            <Button onPress={() => navigation.goBack()} title="Go back home" />
+        </View>
+    );
 }
 
 function AccountScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    </View>
-  );
+    return (
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        ></View>
+    );
 }
 
 function SignOutButton({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    </View>
-  );
+    return (
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        ></View>
+    );
 }
 
 const Drawer = createDrawerNavigator();
 
 export default function HomeStack() {
-  return (
-  
-      <Drawer.Navigator initialRouteName="Home" screenOptions={{headerTitleAlign:'center'}} >
-        <Drawer.Screen name="GuideWalker" component={HomeScreen} />
-        <Drawer.Screen name="Account" component={AccountScreen} />
-        <Drawer.Screen name="Create Tour" component={CreateTourScreen} />
-        <Drawer.Screen name="Sign Out" component={SignOutButton} />
-      </Drawer.Navigator>
-       
- 
-  );
-  
+    return (
+        <Drawer.Navigator
+            initialRouteName="Home"
+            screenOptions={{ headerTitleAlign: 'center' }}
+        >
+            <Drawer.Screen name="GuideWalker" component={HomeScreen} />
+            <Drawer.Screen name="Account" component={AccountScreen} />
+            <Drawer.Screen name="Create Tour" component={CreateTourScreen} />
+            <Drawer.Screen name="Log Out" component={SignOut} />
+        </Drawer.Navigator>
+    );
 }
-
